2d7fb84f0cfbc5af592e22f02ecd0125dcdbaf01
Jaider-Duvan <jaiderduvan961@gmail.com>
2024-05-08 23:14:24 -0500

    Se suben los cambios de respuesta del bot y los estilos de respuesta del usuario y el bot

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const mensajeIngresado = document.getElementById("mensaje-ingresado"); // Cambié el nombre del ID a "mensaje-ingresado"
-const chatBox = document.getElementById("chat-box"); // Cambié el nombre del ID a "chat-box"
+const mensajeIngresado = document.getElementById("mensaje-ingresado"); 
+const chatBox = document.getElementById("chat-box"); 
 
 // Función enviar mensaje
 function sendMessage() {
@@ -41,31 +41,39 @@ function generateBotResponse(userInput) {
     // Variable para almacenar la respuesta del chatbot
     let botResponse;
 
-    // Se evalúa la entrada del usuario y se genera una respuesta adecuada
-    switch (input) {
-        case "1":
-        case "ayuda":
-            botResponse = "Puedo ayudarte con diversas consultas. ¿En qué necesitas ayuda? (Por ejemplo: Ayuda con Drive, compartir un documento, crear una carpeta)";
-            break;
-        case "2":
-        case "Ayuda con Drive":
-            botResponse = "Claro por favor inngresa la duda que tienes ";
-            break;
-        case "3":
-        case "compartir-documento":
-            botResponse = "Si quieres compartir un documento para trabajar con otra persona ingresa 1 si quieres compartir via gmail ingresa 2.";
-            break;
-        case "4":
-        case "Crear-carpeta":
-            botResponse = "Para crear una carpeta ingresa a Drive, en la parte izquierda dale en nuevo y nueva carpeta, ponle un nombre a tu carpeta y dale aceptar o ingresa a esta pagina y te damos mas informacion ";
-            break;
-        default:
-            botResponse = "Lo siento, no entendí tu consulta. Por favor, ingresa el número correspondiente o una palabra clave válida.";
+    // Mensaje inicial con las opciones disponibles
+    if (input === "" || isNaN(input)) {
+        botResponse = "¡Hola! Soy un chatbot. Estaré para ayudarte, Por favor, elige una opción:\n" +
+                      "1. Eliminar Documento\n" +
+                      "2. Subir archivos \n" +
+                      "3. Compartir documento\n" +
+                      "4. Crear carpeta";
+    } else {
+        // Utilizamos un switch-case para manejar diferentes tipos de consultas del usuario
+        switch (input) {
+            case "1":
+            case "Eliminar Documento":
+                botResponse = "Para eliminar un documento necesitas seguir los siguientes pasos: \n 1. Seleccionar el archivo o carpeta a eliminar \n 2. Da click derecho \n 3. Seleciona la opcion mover a la papelera \n 4. Aceptar ";
+                break;
+            case "2":
+            case "Subir archivo":
+                botResponse = " Para subir un archivo sigue los siguientes pasos: \n 1. en la parte izquierda superior dale click en nuevo \n 2. Selecciona la opcion subir archivo \n 3. Selecciona tu archivo \n 4.Dale abrir \n Felicidades tu archivo ya esta en la nube";
+                break;
+            case "3":
+            case "compartir documento":
+                botResponse = "Para compartir un documento sigue los siguientes pasos: \n 1. Selecciona el archivo a compartir \n 2. Da click derecho \n3. Selecciona compartir , en este punto puedes escoger 2 opciones si compartir mediante enlace o agregar colaboradores. \n4. Si Das click a copiar enlace, este se copiara directamente en tu portapapeles. \n5. Para añadir colaboradores da Click a 'Compartir' e ingresa los correos a los cuales quieres compartir el archivo ";
+                break;
+            case "4":
+            case "crear carpeta":
+                botResponse = "Para crear una carpeta sigue los siguientes pasos:\n1. en la parte izquierda dale en 'Nuevo'\n 2. 'Nueva carpeta', ponle un nombre a tu carpeta \n3. dale 'Aceptar'.";
+                break;
+            default:
+                botResponse = "Lo siento, no entendí tu consulta. Estoy aprendiendo cada vez mas para solucionar tus problemas";
+        }
     }
 
     return botResponse;
 }
-
 
 const enviarMensajeBtn = document.getElementById("enviar-mensaje"); 
 enviarMensajeBtn.addEventListener("click", sendMessage);
